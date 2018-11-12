498b825ceeb90be87ee487ab57e10ec6c88b31cd
maruthisivaprasad <vkmsivaprasad@gmail.com>
2018-11-12 05:52:45 +0530

    modifyt edata

--- admin/events.php
+++ admin/events.php
@@ -1,8 +1,117 @@
 <?php require_once('head.php');?>
+<script>
+    var editor; // use a global for the submit and return data rendering in the examples
+ 
+$(document).ready(function() {
+    editor = new $.fn.dataTable.Editor( {
+        "ajax": "../php/staff.php",
+        "table": "#example",
+        "fields": [ {
+                "label": "First name:",
+                "name": "first_name"
+            }, {
+                "label": "Last name:",
+                "name": "last_name"
+            }, {
+                "label": "Position:",
+                "name": "position"
+            }, {
+                "label": "Office:",
+                "name": "office"
+            }, {
+                "label": "Extension:",
+                "name": "extn"
+            }, {
+                "label": "Start date:",
+                "name": "start_date",
+                "type": "datetime"
+            }, {
+                "label": "Salary:",
+                "name": "salary"
+            }
+        ]
+    } );
+ 
+    // New record
+    $('a.editor_create').on('click', function (e) {
+        e.preventDefault();
+ 
+        editor.create( {
+            title: 'Create new record',
+            buttons: 'Add'
+        } );
+    } );
+ 
+    // Edit record
+    $('#example').on('click', 'a.editor_edit', function (e) {
+        e.preventDefault();
+ 
+        editor.edit( $(this).closest('tr'), {
+            title: 'Edit record',
+            buttons: 'Update'
+        } );
+    } );
+ 
+    // Delete a record
+    $('#example').on('click', 'a.editor_remove', function (e) {
+        e.preventDefault();
+ 
+        editor.remove( $(this).closest('tr'), {
+            title: 'Delete record',
+            message: 'Are you sure you wish to remove this record?',
+            buttons: 'Delete'
+        } );
+    } );
+ 
+    $('#example').DataTable( {
+        ajax: "../php/staff.php",
+        columns: [
+            { data: null, render: function ( data, type, row ) {
+                // Combine the first and last names into a single table field
+                return data.first_name+' '+data.last_name;
+            } },
+            { data: "position" },
+            { data: "office" },
+            { data: "extn" },
+            { data: "start_date" },
+            { data: "salary", render: $.fn.dataTable.render.number( ',', '.', 0, '$' ) },
+            {
+                data: null,
+                className: "center",
+                defaultContent: '<a href="" class="editor_edit">Edit</a> / <a href="" class="editor_remove">Delete</a>'
+            }
+        ]
+    } );
+} );
+</script>
 <body>
     <?php require_once('menu.php');?>
     <div class="main">
-        <h2>Sidenav Example</h2>
-        <p>This event is always shown.</p>
+        <a class="editor_create">Create new record</a>
+ 
+<table id="example" class="display" cellspacing="0" width="100%">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Position</th>
+                            <th>Office</th>
+                            <th>Extn.</th>
+                            <th>Start date</th>
+                            <th>Salary</th>
+                            <th>Edit / Delete</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <th>Name</th>
+                            <th>Position</th>
+                            <th>Office</th>
+                            <th>Extn.</th>
+                            <th>Start date</th>
+                            <th>Salary</th>
+                            <th>Edit / Delete</th>
+                        </tr>
+                    </tfoot>
+                </table>
     </div>
 </body>
